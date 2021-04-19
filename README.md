cbfaf56bcf4131bb699c0b7a9ceffa019a19ba0e
Sherin Shaji <gb2852@wayne.edu>
2021-04-19 00:39:22 -0400

    added common users, top tags, positive users, poor images, and inactive users

---
+++ README.md
@@ -0,0 +1,10 @@
+# 4710Project
+
+Sherin Shaji gb2852
+
+Ashley John go2213
+
+
+
+### project part 2 video
+https://rumble.com/vexng9-project-part-two.html
---
+++ database/src/Tag.java
@@ -0,0 +1,17 @@
+import java.lang.String;
+
+public class Tag {
+    protected String tag;
+
+    public Tag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
